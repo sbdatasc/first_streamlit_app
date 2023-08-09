49abfd6c390e32d27dda4d2170e8d8e29801d2f9
Saurav <saurav.bakshi@gmail.com>
2023-08-10 09:39:17 +1000

    Update streamlit_app.py
    Aligned code as per snowflake ESS-DABW lessons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("My Parents New Healthy Diner")
 st.header("Breakfast Menu")
 
-st.write("Omega 3 & Blueberry Oatmeal")
-st.write("Kale , Spinach & Rocket Smoothie")
-st.write("Hard-Boiled Free-Range Egg")
+st.text("Omega 3 & Blueberry Oatmeal")
+st.text("Kale , Spinach & Rocket Smoothie")
+st.text("Hard-Boiled Free-Range Egg")
