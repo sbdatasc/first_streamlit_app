097cc6077b65711d4509ca6298e91141f4f263e2
Saurav <saurav.bakshi@gmail.com>
2023-08-10 09:50:30 +1000

    Update streamlit_app.py
    correct reference to pandas as pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ st.text(" 🥑🍞 Avacado Toast")
 
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
