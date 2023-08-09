8ae27f1f21e0d0a6b30bc8ac0eccd5096e5c2d6d
Saurav <saurav.bakshi@gmail.com>
2023-08-10 09:54:51 +1000

    Update streamlit_app.py
    added user interaction

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,6 @@ st.text(" 🥑🍞 Avacado Toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# display the table on the page and allow user to select fruits
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
