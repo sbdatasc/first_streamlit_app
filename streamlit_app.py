60b19d5e26e52a446112176262306958d2ede89c
Saurav <saurav.bakshi@gmail.com>
2023-08-10 11:52:23 +1000

    Update streamlit_app.py
    fixed a missing comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # display the table on the page and allow user to select fruits
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.multiselect("Pick some fruits:", list(my_fruit_list.index)['Avocado', 'Strawberries'])
+st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 st.dataframe(my_fruit_list)
