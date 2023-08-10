d4e98142c79d3529be5dd72636fcb4040ac7ceb7
Saurav <saurav.bakshi@gmail.com>
2023-08-10 21:11:04 +1000

    Update streamlit_app.py
    adding capability for users to select

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,11 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #st.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
