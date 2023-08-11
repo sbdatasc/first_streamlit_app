6bbdddf04212c4ea6344450773e10aa678f4259a
Saurav <saurav.bakshi@gmail.com>
2023-08-11 18:40:29 +1000

    Update streamlit_app.py
    add try except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import snowflake.connector
+import requests
+from urllib.error import URLError
 
 
 st.title("My Parents New Healthy Diner")
@@ -24,18 +26,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+  st.error()
+  
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+    
+# st.write('The user entered ', fruit_choice)
+    
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# import requests
+
 #st.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
 
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
@@ -51,7 +63,8 @@ add_my_fruit = st.text_input('What fruit would you like information about?')
 
 str_add_choice = "".join([s for s in add_my_fruit])
 st.write('The user entered ', str_add_choice)
-my_data_rows.append(str_add_choice)
+#my_data_rows.append(str_add_choice)
 st.dataframe(my_data_rows)
 st.text("Thanks for adding " + str_add_choice)
+
 
