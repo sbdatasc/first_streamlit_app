37ac05ace964a540a9f9d89d5fadb4931cdd76c4
Saurav <saurav.bakshi@gmail.com>
2023-08-12 12:48:51 +1000

    Update streamlit_app.py
    added my connection close statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ def get_fruit_load_list():
 if st.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   st.dataframe(my_data_rows)
   
 def insert_row_snowflake(new_fruit):
@@ -77,6 +78,7 @@ add_my_fruit = st.text_input('What fruit would you like to add?', 'Add fruit to 
 if st.button('Add a Fruit to the list'):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   st.text(back_from_function)
   
   
